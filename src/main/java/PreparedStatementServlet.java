10bd8649f125d568b6a4a5e3b9cdc5785a3cae4f
Hidetoshi Imai <hidepin@gmail.com>
2017-07-20 20:33:04 +0900

    Rename Servlet.

--- src/main/java/PreparedStatementServlet.java
+++ src/main/java/PreparedStatementServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
-public class PrepareStatementServlet extends HttpServlet {
+public class PreparedStatementServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
         response.setContentType("text/html; charset=utf-8");
