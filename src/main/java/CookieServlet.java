2e86bd32a4d06fd4f0233c3cc2096e16c95b786e
Hidetoshi Imai <hidepin@gmail.com>
2018-10-17 22:42:06 +0900

    Add format.

--- src/main/java/CookieServlet.java
+++ src/main/java/CookieServlet.java
@@ -1,109 +1,113 @@
 import java.io.*;
-import javax.servlet.http.*;
 import java.io.IOException;
 import javax.servlet.ServletException;
-import java.net.InetAddress;
+import javax.servlet.http.*;
 
 public class CookieServlet extends HttpServlet {
 
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    private static String sessionKey = "counter";
+  private static String sessionKey = "counter";
 
-    public void doGet(HttpServletRequest request,
-                      HttpServletResponse response)
-        throws ServletException, IOException {
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
 
-        HttpSession session = request.getSession(true);
-        Cookie cookie[] = request.getCookies();
+    HttpSession session = request.getSession(true);
+    Cookie cookie[] = request.getCookies();
 
-        int counter = 1;
-        if (session.getAttribute(sessionKey) != null) {
-            counter = ((Integer)session.getAttribute(sessionKey)).intValue();
-        }
-
-        PrintWriter out = response.getWriter();
-
-        response.setContentType("text/html; charset=utf-8");
-
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<meta charset=\"utf-8\">");
-        out.println("<title>Cookie Sevlet GET</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h2>[AP Server]: " + request.getServerName()+ "</h2>");
-        if (cookie != null){
-            for (int i = 0 ; i < cookie.length ; i++){
-                out.println("<h2>[Cookie Name]:" + cookie[i].getName() + " [Cookie Value]:" + cookie[i].getValue() + "</h2>");
-            }
-        }
-        out.println("<h2>[Counter]: " + counter + "</h2>");
-        out.println("<form method=\"GET\" action=\""
-                    + request.getContextPath() + "/NormalServlet\">");
-        out.println("<input type=\"submit\" value =\"normal\">" );
-        out.println("</form>");
-        out.println("<form method=\"GET\" action=\""
-                    + request.getContextPath() + "/CookieServlet\">");
-        out.println("<input type=\"submit\" value =\"view counter\">" );
-        out.println("</form>");
-        out.println("<form method=\"POST\" action=\""
-                    + request.getContextPath() + "/CookieServlet\">");
-        out.println("<input type=\"submit\" value =\"count up\">" );
-        out.println("</form>");
-        out.println("</body>");
-        out.println("</html>");
+    int counter = 1;
+    if (session.getAttribute(sessionKey) != null) {
+      counter = ((Integer) session.getAttribute(sessionKey)).intValue();
     }
 
-    public void doPost(HttpServletRequest request,
-                      HttpServletResponse response)
-        throws ServletException, IOException {
+    PrintWriter out = response.getWriter();
 
-        HttpSession session = request.getSession(true);
-        Cookie cookie[] = request.getCookies();
+    response.setContentType("text/html; charset=utf-8");
 
-        int counter = 1;
-        if (session.getAttribute(sessionKey) != null) {
-            counter = ((Integer)session.getAttribute(sessionKey)).intValue();
-            counter++;
-        }
-        session.setAttribute(sessionKey,Integer.valueOf(counter));
-
-        PrintWriter out = response.getWriter();
-
-        response.setContentType("text/html; charset=utf-8");
-
-        response.setContentType("text/html; charset=utf-8");
-
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<meta charset=\"utf-8\">");
-        out.println("<title>Cookie Sevlet GET</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h2>[AP Server]: " + request.getServerName()+ "</h2>");
-        if (cookie != null){
-            for (int i = 0 ; i < cookie.length ; i++){
-                out.println("<h2>[Cookie Name]:" + cookie[i].getName() + " [Cookie Value]:" + cookie[i].getValue() + "</h2>");
-            }
-        }
-        out.println("<h2>[Counter]: " + counter + "</h2>");
-        out.println("<form method=\"GET\" action=\""
-                    + request.getContextPath() + "/NormalServlet\">");
-        out.println("<input type=\"submit\" value =\"normal\">" );
-        out.println("</form>");
-        out.println("<form method=\"GET\" action=\""
-                    + request.getContextPath() + "/CookieServlet\">");
-        out.println("<input type=\"submit\" value =\"view counter\">" );
-        out.println("</form>");
-        out.println("<form method=\"POST\" action=\""
-                    + request.getContextPath() + "/CookieServlet\">");
-        out.println("<input type=\"submit\" value =\"count up\">" );
-        out.println("</form>");
-        out.println("</body>");
-        out.println("</html>");
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<meta charset=\"utf-8\">");
+    out.println("<title>Cookie Sevlet GET</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h2>[AP Server]: " + request.getServerName() + "</h2>");
+    if (cookie != null) {
+      for (int i = 0; i < cookie.length; i++) {
+        out.println(
+            "<h2>[Cookie Name]:"
+                + cookie[i].getName()
+                + " [Cookie Value]:"
+                + cookie[i].getValue()
+                + "</h2>");
+      }
     }
+    out.println("<h2>[Counter]: " + counter + "</h2>");
+    out.println("<form method=\"GET\" action=\"" + request.getContextPath() + "/NormalServlet\">");
+    out.println("<input type=\"submit\" value =\"normal\">");
+    out.println("</form>");
+    out.println("<form method=\"GET\" action=\"" + request.getContextPath() + "/CookieServlet\">");
+    out.println("<input type=\"submit\" value =\"view counter\">");
+    out.println("</form>");
+    out.println("<form method=\"POST\" action=\"" + request.getContextPath() + "/CookieServlet\">");
+    out.println("<input type=\"submit\" value =\"count up\">");
+    out.println("</form>");
+    out.println("</body>");
+    out.println("</html>");
+  }
 
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    HttpSession session = request.getSession(true);
+    Cookie cookie[] = request.getCookies();
+
+    int counter = 1;
+    if (session.getAttribute(sessionKey) != null) {
+      counter = ((Integer) session.getAttribute(sessionKey)).intValue();
+      counter++;
+    }
+    session.setAttribute(sessionKey, Integer.valueOf(counter));
+
+    PrintWriter out = response.getWriter();
+
+    response.setContentType("text/html; charset=utf-8");
+
+    response.setContentType("text/html; charset=utf-8");
+
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<meta charset=\"utf-8\">");
+    out.println("<title>Cookie Sevlet GET</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h2>[AP Server]: " + request.getServerName() + "</h2>");
+    if (cookie != null) {
+      for (int i = 0; i < cookie.length; i++) {
+        out.println(
+            "<h2>[Cookie Name]:"
+                + cookie[i].getName()
+                + " [Cookie Value]:"
+                + cookie[i].getValue()
+                + "</h2>");
+      }
+    }
+    out.println("<h2>[Counter]: " + counter + "</h2>");
+    out.println("<form method=\"GET\" action=\"" + request.getContextPath() + "/NormalServlet\">");
+    out.println("<input type=\"submit\" value =\"normal\">");
+    out.println("</form>");
+    out.println("<form method=\"GET\" action=\"" + request.getContextPath() + "/CookieServlet\">");
+    out.println("<input type=\"submit\" value =\"view counter\">");
+    out.println("</form>");
+    out.println("<form method=\"POST\" action=\"" + request.getContextPath() + "/CookieServlet\">");
+    out.println("<input type=\"submit\" value =\"count up\">");
+    out.println("</form>");
+    out.println("</body>");
+    out.println("</html>");
+  }
+
+  private void hello() {
+    System.out.println("hello");
+  }
 }
